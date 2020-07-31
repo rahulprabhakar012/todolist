fdecece5efadccc46da939aaa275399064612c4f
RAHUL PRABHAKAR <rahulprabhakar012@gmail.com>
2020-08-01 01:19:57 +0530

    Updates

---
+++ precache-manifest.8970d5d1d29d534c489edc6da55bf648.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb43ca7ed1833fd0ce2b48da065bfbb",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "fee4dd21f7d339350e42",
+    "url": "/todolist/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "b951b072d2a9c0ffc417",
+    "url": "/todolist/static/css/main.c60c0fe0.chunk.css"
+  },
+  {
+    "revision": "fee4dd21f7d339350e42",
+    "url": "/todolist/static/js/2.0516036c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todolist/static/js/2.0516036c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b951b072d2a9c0ffc417",
+    "url": "/todolist/static/js/main.7062aa7b.chunk.js"
+  },
+  {
+    "revision": "81be2d8b59830bd533eb",
+    "url": "/todolist/static/js/runtime-main.1cae17d7.js"
+  }
+]);
